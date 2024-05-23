3487b7927b76d55135ddd1b3406651cabe0df046
Jonathan Stollberg <jonathanstollberg@gmail.com>
2024-05-23 13:57:35 +0200

    added comments on what to do with this example

--- examples/matlab_lattice_input/main.py
+++ examples/matlab_lattice_input/main.py
@@ -1,24 +1,26 @@
 import os
 import sys
 
-# import gmsh and homogenization package
+# generate input file for the matlab homogenization tool available from:
+# https://www.mathworks.com/matlabcentral/fileexchange/67457-3d-homogenization-of-cellular-materials
+
 base_path = "C:/Users/jonat/Documents/"
 gmsh_path = os.path.join(base_path, "gmsh", "lib")
-abqhom_path = os.path.join(base_path, 
-                            "Institute for Mechanics", 
-                            "abqhom", 
-                            "src")
+abqhom_path = os.path.join(base_path,
+                           "abqhom",
+                           "src")
 sys.path.append(gmsh_path)
 sys.path.append(abqhom_path)
 from abqhom.RVE import write_matlab_input, finalize_model
 from abqhom.examples import simple_RVE_3d
 import gmsh
 
+# RVE size and strut length
 dx = dy = dz = 10
 lc = 5
 
+# write input file
 model_name, group_map = simple_RVE_3d(dx=dx, dy=dy, dz=dz, lc=lc)
-gmsh.fltk.run()
-
 write_matlab_input(model_name, "stochastic.txt")
+# gmsh.fltk.run()
 finalize_model()
